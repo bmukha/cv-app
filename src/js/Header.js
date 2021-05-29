a80145aeffe5c1b65c0d46846f4a5d24322390e5
bmukha <47007157+bmukha@users.noreply.github.com>
2021-05-29 21:45:51 +0300

    Initial commit

---
+++ src/js/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Header.css"
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>CV app for The Odin Project</h1>
+    </header>
+  );
+}
+
+export default Header;
